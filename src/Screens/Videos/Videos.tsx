9dee5cb44a79181d97daf534b2562f8726873568
aleksandern NA <aleksandern08@gmail.com>
2021-02-19 19:01:40 +0700

    position fix

--- src/Screens/Videos/Videos.tsx
+++ src/Screens/Videos/Videos.tsx
@@ -10,10 +10,6 @@ import _ from 'lodash';
 import {
   VideoItem,
 } from 'src/Common/Types/VideoTypes';
-import {
-  useDeviceOrientation,
-  usePrevious,
-} from 'src/Common/hooks';
 
 import dataVideos from 'src/Fixtures/simple-feed.json';
 
@@ -22,30 +18,8 @@ import VideosPlayer from './VideosPlayer';
 const Videos = () => {
   const listRef = React.useRef<VirtualizedList<VideoItem> | null>(null);
   const { width, height }  = useWindowDimensions();
-  const deviceOrientation = useDeviceOrientation();
   const [currentIndex, setCurrentIndex] = React.useState<number | null>(0);
   const { data } = dataVideos;
-
-  const deviceOrientationPrev = usePrevious(deviceOrientation);
-  React.useEffect(() => {
-    if (_.isNil(deviceOrientationPrev)) {
-      return;
-    }
-
-    if (deviceOrientationPrev === deviceOrientation) {
-      return;
-    }
-
-    if (currentIndex !== null) {
-      setTimeout(() => {
-        listRef.current?.scrollToIndex({
-          index: currentIndex,
-          animated: false,
-        });
-      }, 100);
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [deviceOrientation]);
 
   const renderItem = ({ item, index  }: { item: VideoItem, index: number }) => {
     const isActive = (currentIndex === index);
@@ -73,6 +47,9 @@ const Videos = () => {
   return (
     <VirtualizedList
       ref={listRef}
+      maintainVisibleContentPosition={{
+        minIndexForVisible: 0,
+      }}
       initialNumToRender={3}
       maxToRenderPerBatch={3}
       keyExtractor={({ id }) => id}
